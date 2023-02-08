609b03e467d3ce6a1a9e97cb81e44db4abeccb63
zerohertz <ohg3417@gmail.com>
2023-02-08 17:46:10 +0900

    :hammer: Update: result_format.py for PAN_PP_TEST

--- utils/result_format.py
+++ utils/result_format.py
@@ -1,12 +1,14 @@
 import os
 import os.path as osp
 import zipfile
-
+import cv2
+import numpy as np
+import datetime
 
 class ResultFormat(object):
     def __init__(self, data_type, result_path):
         self.data_type = data_type
-        self.result_path = result_path
+        self.result_path = os.path.join(result_path,str(datetime.datetime.now()).replace('.','').replace(' ','-'))
 
         if osp.isfile(result_path):
             os.remove(result_path)
@@ -14,104 +16,34 @@ class ResultFormat(object):
         if result_path.endswith('.zip'):
             result_path = result_path.replace('.zip', '')
 
-        if not osp.exists(result_path):
-            os.makedirs(result_path)
+        if not osp.exists(self.result_path):
+            os.makedirs(self.result_path)
 
-    def write_result(self, img_metas, outputs):
-        img_name = img_metas['img_name'][0]
-        if 'IC15' in self.data_type:
-            self._write_result_ic15(img_name, outputs)
-        elif 'TT' in self.data_type:
-            self._write_result_tt(img_name, outputs)
-        elif 'CTW' in self.data_type:
-            self._write_result_ctw(img_name, outputs)
-        elif 'MSRA' in self.data_type:
-            self._write_result_msra(img_name, outputs)
+    def write_result(self, img_name,image_path, outputs):
+        self._write_result_ic15(img_name,image_path, outputs)
 
-    def _write_result_ic15(self, img_name, outputs):
-        assert self.result_path.endswith(
-            '.zip'), 'Error: ic15 result should be a zip file!'
-
+    def _write_result_ic15(self, img_name,image_path, outputs):
         tmp_folder = self.result_path.replace('.zip', '')
 
         bboxes = outputs['bboxes']
+        scores=outputs['scores']
         words = None
         if 'words' in outputs:
             words = outputs['words']
-
-        lines = []
-        for i, bbox in enumerate(bboxes):
-            values = [int(v) for v in bbox]
-            if words is None:
-                line = '%d,%d,%d,%d,%d,%d,%d,%d\n' % tuple(values)
-                lines.append(line)
-            elif words[i] is not None:
-                line = '%d,%d,%d,%d,%d,%d,%d,%d' % tuple(
-                    values) + ',%s\n' % words[i]
-                lines.append(line)
-
-        file_name = 'res_%s.txt' % img_name
+        print()
+        img=cv2.imread(image_path)
+        file_name = '%s.txt' % img_name
         file_path = osp.join(tmp_folder, file_name)
         with open(file_path, 'w') as f:
-            for line in lines:
-                f.write(line)
+            for i, bbox in enumerate(bboxes):        
+                poly = np.array(bbox).astype(np.int32).reshape((-1))
+                poly[poly<0]=0
+                strResult = '\t'.join([str(p) for p in poly]) #+ ',' + str(scores[i]) 
+                result=strResult + '\r\n'
+                f.write(result)
+                #f.write(str(scores[i]))
+                poly = poly.reshape(-1, 2)
+                cv2.polylines(img, [poly.reshape((-1, 1, 2))], True, color=(0, 0, 255), thickness=2)
+        result_name='%s.jpg' % img_name
+        cv2.imwrite(os.path.join(self.result_path,result_name), img)
 
-        z = zipfile.ZipFile(self.result_path, 'a', zipfile.ZIP_DEFLATED)
-        z.write(file_path, file_name)
-        z.close()
-
-    def _write_result_tt(self, image_name, outputs):
-        bboxes = outputs['bboxes']
-
-        lines = []
-        for i, bbox in enumerate(bboxes):
-            bbox = bbox.reshape(-1, 2)[:, ::-1].reshape(-1)
-            values = [int(v) for v in bbox]
-            line = '%d' % values[0]
-            for v_id in range(1, len(values)):
-                line += ',%d' % values[v_id]
-            line += '\n'
-            lines.append(line)
-
-        file_name = '%s.txt' % image_name
-        file_path = osp.join(self.result_path, file_name)
-        with open(file_path, 'w') as f:
-            for line in lines:
-                f.write(line)
-
-    def _write_result_ctw(self, image_name, outputs):
-        bboxes = outputs['bboxes']
-
-        lines = []
-        for i, bbox in enumerate(bboxes):
-            bbox = bbox.reshape(-1, 2)[:, ::-1].reshape(-1)
-            values = [int(v) for v in bbox]
-            line = '%d' % values[0]
-            for v_id in range(1, len(values)):
-                line += ',%d' % values[v_id]
-            line += '\n'
-            lines.append(line)
-
-        file_name = '%s.txt' % image_name
-        file_path = osp.join(self.result_path, file_name)
-        with open(file_path, 'w') as f:
-            for line in lines:
-                f.write(line)
-
-    def _write_result_msra(self, image_name, outputs):
-        bboxes = outputs['bboxes']
-    
-        lines = []
-        for b_idx, bbox in enumerate(bboxes):
-            values = [int(v) for v in bbox]
-            line = '%d' % values[0]
-            for v_id in range(1, len(values)):
-                line += ', %d' % values[v_id]
-            line += '\n'
-            lines.append(line)
-    
-        file_name = '%s.txt' % image_name
-        file_path = osp.join(self.result_path, file_name)
-        with open(file_path, 'w') as f:
-            for line in lines:
-                f.write(line)
