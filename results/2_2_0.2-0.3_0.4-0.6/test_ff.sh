94d2df883fe6d03eddbaa8d17aaf010a8775b838
zerohertz <ohg3417@gmail.com>
2023-02-15 13:08:42 +0900

    :white_check_mark: Add: Experiment 2 (2/0.2~0.3/0.4~0.6)

---
+++ results/2_2_0.2-0.3_0.4-0.6/test_ff.sh
@@ -0,0 +1,42 @@
+cd outputs
+rm -rf 20*
+cd ..
+cd time
+rm -rf tmp.csv
+cd ..
+sh compile.sh
+
+exe(){
+    echo "resize_const: " ${resize_const}
+    echo "pos_const: " ${pos_const}
+    echo "len_const: " ${len_const}
+
+    CUDA_VISIBLE_DEVICES=3 python test.py config/pan_pp/pan_pp_test.py --resize_const=$resize_const --pos_const=$pos_const --len_const=$len_const
+
+    cd time
+    mv ./tmp.csv ./Ver3_${resize_const}_${pos_const}_${len_const}.csv
+    cd ..
+
+    cd outputs
+    mv ./20* ./Ver3_${resize_const}_${pos_const}_${len_const}
+    cd ..
+}
+
+for rc in '2'
+do
+    for pc in '0.2' '0.25' '0.3'
+    do
+        for lc in '0.4' '0.5' '0.6'
+        do
+            echo $rc $pc $lc
+            resize_const=$rc
+            pos_const=$pc
+            len_const=$lc
+            exe ${resize_const} ${pos_const} ${len_const}
+        done
+    done
+done
+
+cd time
+rm -rf time.csv
+python Time_Measurement.py
