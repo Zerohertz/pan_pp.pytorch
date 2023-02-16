bfff2b077fb40f15d863c91ed38802a5a8dad30a
zerohertz <ohg3417@gmail.com>
2023-02-16 16:58:14 +0900

    :zap: Update: test.sh

--- test.sh
+++ test.sh
@@ -11,32 +11,10 @@ exe(){
     echo "pos_const: " ${pos_const}
     echo "len_const: " ${len_const}
 
-    CUDA_VISIBLE_DEVICES=3 python test.py config/pan_pp/pan_pp_test.py --resize_const=$resize_const --pos_const=$pos_const --len_const=$len_const
-
-    cd time
-    mv tmp.csv Ver3_${resize_const}_${pos_const}_${len_const}.csv
-    cd ..
-
-    cd outputs
-    mv 20* Ver3_${resize_const}_${pos_const}_${len_const}
-    cd ..
+    CUDA_VISIBLE_DEVICES=7 python test.py config/pan_pp/pan_pp_test.py --resize_const=$resize_const --pos_const=$pos_const --len_const=$len_const
 }
 
-for rc in '2' '3' '4'
-do
-    for pc in '0' '0.1' '0.2'
-    do
-        for lc in '0' '0.5' '1'
-        do
-            echo $rc $pc $lc
-            resize_const=$rc
-            pos_const=$pc
-            len_const=$lc
-            exe ${resize_const} ${pos_const} ${len_const}
-        done
-    done
-done
-
-cd time
-rm -rf time.csv
-python Time_Measurement.py
+read resize_const
+read pos_const
+read len_const
+exe ${resize_const} ${pos_const} ${len_const}
