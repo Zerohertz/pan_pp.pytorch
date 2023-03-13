1e38284fc7d4f01e5f7a5ec0203f2d1f940c0a2a
zerohertz <ohg3417@gmail.com>
2023-03-13 15:19:31 +0900

    :fire: Update: Time Measurement

--- results/time/Time_Measurement.py
+++ results/time/Time_Measurement.py
@@ -8,7 +8,7 @@ if 'time.csv' in os.listdir():
 for i in os.listdir():
     if '.csv' in i and not i == 'time.csv':
         data = pd.read_csv(i, header=None)
-        tot = 0
+        tmp = data.mean() * 1000
         with open('./time.csv', 'a', encoding='utf8') as f:
             wr = csv.writer(f)
-            wr.writerow([i[0:-4], data[0].mean()*1000, data[1].mean()*1000, data[2].mean()*1000, data[3].mean()*1000, data[4].mean()*1000, data[5].mean()*1000])
+            wr.writerow([i[0:-4], *tmp])
