7f4668febb3f6cfdc10f88c635e8510d3a197cc5
Zerohertz <ohg3417@gmail.com>
2023-03-21 09:38:52 +0900

    :memo: Update: README.md

--- train.sh
+++
@@ -1,9 +0,0 @@
-# Compile Cython
-cd ./models/post_processing/pa/
-python setup.py build_ext --inplace
-cd ../boxgen/
-python setup.py build_ext --inplace
-cd ../../../
-echo Done!
-
-CUDA_VISIBLE_DEVICES=1,3,4,5,7 python train.py config/pan_pp/pan_pp_test.py
